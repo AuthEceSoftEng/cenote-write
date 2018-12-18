7c7ca5923bd8268f4273bc910141f8775a1c0628
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2018-12-18 16:33:20 +0200

    Update conf

--- src/main/java/com/issel/cenote/WriteTopology.java
+++ src/main/java/com/issel/cenote/WriteTopology.java
@@ -15,19 +15,19 @@ import org.apache.storm.topology.TopologyBuilder;
 
 public class WriteTopology {
     public static void main(String[] args) {
-        Config config = new Config();
-        config.setDebug(true);
-        config.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
         BrokerHosts hosts = new ZkHosts("83.212.104.172:2181,83.212.104.177:2181,83.212.96.15:2181", "/brokers");
         SpoutConfig kafkaSpoutConfig = new SpoutConfig(hosts, "cenoteIncoming", "/brokers/topics/" + "cenoteIncoming", UUID.randomUUID().toString());
-        kafkaSpoutConfig.bufferSizeBytes = 1024 * 1024 * 4;
-        kafkaSpoutConfig.fetchSizeBytes = 1024 * 1024 * 4;
+        kafkaSpoutConfig.bufferSizeBytes = 1024 * 1024 * 100;
+        kafkaSpoutConfig.fetchSizeBytes = 1024 * 1024 * 100;
         kafkaSpoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("kafka-spout", new KafkaSpout(kafkaSpoutConfig), 8);
-        builder.setBolt("forwardToCassandra", new WriteToCassandra(), 8).shuffleGrouping("kafka-spout");
-        config.setNumWorkers(5);
+        builder.setSpout("kafka-spout", new KafkaSpout(kafkaSpoutConfig), 15).setNumTasks(4);
+        builder.setBolt("forwardToCassandra", new WriteToCassandra(), 2).setNumTasks(4).shuffleGrouping("kafka-spout");
+        Config config = new Config();
+        config.setMaxTaskParallelism(200);
+        config.setNumWorkers(10);
+        config.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 50);
         try {
             StormSubmitter.submitTopology(args[0], config, builder.createTopology());
         } catch (Exception e) {
